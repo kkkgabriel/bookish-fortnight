cf504bdce44c9bd919925c2bae6a19c867623f07
Raphael Koh <raphael@Raphaels-Mac-mini.local>
2022-04-28 22:38:16 +0800

    first

---
+++ resources/views/another.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<!-- <link rel="stylesheet" href="styles.css"> -->
+	<title>another</title>
+	<style>
+		.special {font-size:  30px; color:  red;}
+	</style>
+</head>
+<body>
+
+	<div class='special'>potato</div>
+
+</body>
+</html>
